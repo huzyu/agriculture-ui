4c01d15365d44638c9f096055ac87fe5516b75c7
abueavan <eavanzhou@gmail.com>
2019-05-27 09:43:00 +0800

    123

--- resource/static/js/eme.js
+++ resource/static/js/eme.js
@@ -1,6 +1,5 @@
 function emergency1() {
-    let fUrl = " /stop/162";
-    info.id = 162 ;
+    let fUrl = "/stop/162";
     jQuery.ajax({
         url:fUrl,
         type:'GET',
@@ -13,8 +12,8 @@ function emergency1() {
 }
 
 function emergency2() {
-    let fUrl = " /stop/163";
-    info.id = 163 ;
+    let fUrl = "/stop/163";
+
     jQuery.ajax({
         url:fUrl,
         type:'GET',
